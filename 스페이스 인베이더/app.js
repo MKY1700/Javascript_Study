507e9e97beae31d7dd9c18b76497f01d55d6e530
MKY1700 <a64051700@gmail.com>
2024-06-24 16:07:48 +0900

    스페이스 인베이더 4

--- 스페이스 인베이더/app.js
+++ 스페이스 인베이더/app.js
@@ -9,7 +9,7 @@ const grid = document.querySelector(".grid");
 const stage = document.querySelectorAll(".grid div");
 const startBtn = document.querySelector("#startBtn");
 const stopBtn = document.querySelector("#stopBtn");
-const display = document.querySelector("#dispSlay");
+const display = document.querySelector("#display");
 
 let playerLoc = stageSize*(stageSize-2)+7
 let invadersLoc = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
@@ -17,10 +17,13 @@ let invadersLoc = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
                     30, 31, 32, 33, 34, 35, 36, 37, 38, 39];
 
 let gameInterval;
+let bulletId;
+let bulletLoc = playerLoc;
 
 function makePlayer() {
     stage[playerLoc].classList.add("player");
 }
+
 function movePlayer(e) {
     stage[playerLoc].classList.remove("player");
     switch (e.keyCode) {
@@ -54,18 +57,18 @@ function gameStart() {
     invadersLoc.forEach(function(invader) {
         stage[invader].classList.remove("invader");
     });
+    display.innerText = ""
     playerLoc = stageSize*(stageSize-2)+7
     invadersLoc = [
         0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
         15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
-        30, 31, 32, 33, 34, 35, 36, 37, 38, 39
-    ]
+        30, 31, 32, 33, 34, 35, 36, 37, 38, 39]
     makePlayer();
     makeInvader();
     displayStatus();
     gameInterval = setInterval(gameRun, 1000);
     document.addEventListener("keyup", movePlayer);
-    gameRun();
+    gameRun()
 }
 
 function gameStop() {
@@ -80,6 +83,26 @@ function gameRun() {
 function displayStatus() {
     display.innerText = invadersLoc.length + "/" + invadersLoc.length
 }
+
+function moveBullet() {
+    stage[bulletLoc].classList.remove("bullet");
+    bulletLoc -= stageSize;
+    stage[bulletLoc].classList.add("bullet");
+}
+
+function shoot(e) {
+    let id;
+    let bulletLoc = playerLoc;
+    function moveBullet() {
+        stage[p].classList.remove("bullet");
+        p -= stageSize;
+        stage[p].classList.add("bullet");
+    }
+    if (e.key === "ArrowUp") {
+        id = setInterval(moveBullet, 300)
+    }
+}
+document.addEventListener("keydown", movePlayer)
 
 startBtn.addEventListener("click", gameStart);
 stopBtn.addEventListener("click", gameStop);
