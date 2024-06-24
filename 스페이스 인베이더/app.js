23ddbe9208fc264e5093b74dc5b4deb57f0979ee
MKY1700 <a64051700@gmail.com>
2024-06-24 15:25:17 +0900

    스페이스 인베이더3

--- 스페이스 인베이더/app.js
+++ 스페이스 인베이더/app.js
@@ -8,16 +8,19 @@ for(let i=0; i<stageSize*stageSize; i++) {
 const grid = document.querySelector(".grid");
 const stage = document.querySelectorAll(".grid div");
 const startBtn = document.querySelector("#startBtn");
-const stopBtn = document.querySelector("#startBtn");
-const display = document.querySelector("#display");
+const stopBtn = document.querySelector("#stopBtn");
+const display = document.querySelector("#dispSlay");
 
 let playerLoc = stageSize*(stageSize-2)+7
+let invadersLoc = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+                    15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+                    30, 31, 32, 33, 34, 35, 36, 37, 38, 39];
+
+let gameInterval;
 
 function makePlayer() {
     stage[playerLoc].classList.add("player");
 }
-makePlayer()
-
 function movePlayer(e) {
     stage[playerLoc].classList.remove("player");
     switch (e.keyCode) {
@@ -31,22 +34,52 @@ function movePlayer(e) {
     stage[playerLoc].classList.add("player");
 }
 
-document.addEventListener("keyup", movePlayer)
-
-let invadersLoc = [
-    0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-    15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
-    30, 31, 32, 33, 34, 35, 36, 37, 38, 39
-]
-let gameInterval;
-
 function makeInvader() {
     invadersLoc.forEach(function(invader) {
         stage[invader].classList.add("invader");
     })
 }
-makeInvader();
+function moveInvader() {
+    invadersLoc.forEach(function(invader) {
+        stage[invader].classList.remove("invader");
+    })
+    for(let i=0; i < invadersLoc.length; i++) {
+        invadersLoc[i]++;
+        stage[invadersLoc[i]].classList.add("invader");
+    }
+}
 
-function moveInvader() {}
+function gameStart() {
+    stage[playerLoc].classList.remove("player");
+    invadersLoc.forEach(function(invader) {
+        stage[invader].classList.remove("invader");
+    });
+    playerLoc = stageSize*(stageSize-2)+7
+    invadersLoc = [
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+        15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+        30, 31, 32, 33, 34, 35, 36, 37, 38, 39
+    ]
+    makePlayer();
+    makeInvader();
+    displayStatus();
+    gameInterval = setInterval(gameRun, 1000);
+    document.addEventListener("keyup", movePlayer);
+    gameRun();
+}
 
-interval = setInterval(moveInvader, 1000);
+function gameStop() {
+    clearInterval(gameInterval);
+    document.removeEventListener("keyup", movePlayer);
+}
+
+function gameRun() {
+    moveInvader();
+}
+
+function displayStatus() {
+    display.innerText = invadersLoc.length + "/" + invadersLoc.length
+}
+
+startBtn.addEventListener("click", gameStart);
+stopBtn.addEventListener("click", gameStop);
